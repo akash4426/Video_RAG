55e00076898d9210bbe64fe616e85808aa6dfceb
akash <akashkarri2006@gmail.com>
2025-10-22 20:49:05 +0530

    Fixed the production issues

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# setup.sh
-# Installs missing system libraries for OpenCV
-
-apt-get update
-apt-get install -y libgl1-mesa-glx libglib2.0-0 libsm6 libxrender1 libxext6
-echo "System libraries for OpenCV installed."
